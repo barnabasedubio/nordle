0d8024cc7e4b53a4a7925535a9ed39a2e69e91b6
Barnabas Edubio <barny.edubio@gmail.com>
2022-06-15 12:45:52 +0200

    add word rows and keyboard to layout

--- src/layout/TheContainer.vue
+++ src/layout/TheContainer.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
-import { useStore } from "../store/store.js"
+import { useStore } from "../store/store.js";
+import WordRow from "../components/WordRow.vue";
+import Keyboard from "../components/Keyboard.vue"
 
-const store = useStore()
-const testValue: String = store.wordList[0]
+const store = useStore();
+const testValue: String = store.todaysWord.toUpperCase();
 </script>
 
 <template>
-<div>
-{{ testValue }}
-</div>
+  {{ testValue }}
+  <div class="flex flex-col justify-center">
+    <div class="sm:mx-auto border-solid border-2 border-blue-500">
+      <WordRow/>
+      <WordRow/>
+      <WordRow/>
+      <WordRow/>
+      <WordRow/>
+      <WordRow/>
+    </div>
+		<div class="sm:mx-auto mt-8 border-solid border-2 border-blue-500">
+			<Keyboard/>
+		</div>
+  </div>
 </template>
