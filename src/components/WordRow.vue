c4cf0d2bf71264988a6fdd88e4b06755bbba2428
Barnabas Edubio <barny.edubio@gmail.com>
2022-06-19 23:26:40 +0200

    color word rows

--- src/components/WordRow.vue
+++ src/components/WordRow.vue
@@ -10,24 +10,33 @@ const props = defineProps({
   },
 });
 
+const rowNumber: number = parseInt(props.row);
+
 function getLetter(index: number) {
-	const rowNumber: number = parseInt(props.row)
   if (rowNumber < store.enteredWords.length) {
-		return store.enteredWords[rowNumber][index]
+    return store.enteredWords[rowNumber][index].toUpperCase();
   }
   if (rowNumber === store.enteredWords.length) {
-		return store.currentWordAsArray[index] || "";
-  } 
-	else return "";
+    return store.currentWordAsArray[index] || "";
+  } else return "";
+}
+
+function matchColor(index: number) {
+  if (rowNumber < store.enteredWords.length) {
+    const color: string = store.matchColors[rowNumber][index];
+    if (color === "green") return "bg-green-300";
+    if (color === "yellow") return "bg-yellow-300";
+    else return "bg-gray-400";
+  } else return "bg-gray-400";
 }
 </script>
 
 <template>
   <div class="flex">
-    <word-row-letter>{{ getLetter(0) }}</word-row-letter>
-    <word-row-letter>{{ getLetter(1) }}</word-row-letter>
-    <word-row-letter>{{ getLetter(2) }}</word-row-letter>
-    <word-row-letter>{{ getLetter(3) }}</word-row-letter>
-    <word-row-letter>{{ getLetter(4) }}</word-row-letter>
+    <word-row-letter :class="matchColor(0)">{{ getLetter(0) }}</word-row-letter>
+    <word-row-letter :class="matchColor(1)">{{ getLetter(1) }}</word-row-letter>
+    <word-row-letter :class="matchColor(2)">{{ getLetter(2) }}</word-row-letter>
+    <word-row-letter :class="matchColor(3)">{{ getLetter(3) }}</word-row-letter>
+    <word-row-letter :class="matchColor(4)">{{ getLetter(4) }}</word-row-letter>
   </div>
 </template>
