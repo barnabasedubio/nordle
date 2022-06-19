1959ea2142c95b7373cceac2fea0e07f35206bb5
Barnabas Edubio <barny.edubio@gmail.com>
2022-06-19 23:24:40 +0200

    add logic to evaluate word tries

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,8 +5,15 @@ import solutionWordList from "../data/solutionWordList.json";
 interface IState {
   validWordList: string[];
   solutionWordList: string[];
+  todaysWord: string;
   currentWordAsArray: string[];
   enteredWords: string[];
+  matchColors: string[][];
+  acceptingInputs: boolean;
+}
+
+interface IWordMap {
+  [letter: string]: number;
 }
 
 export const useStore = defineStore("main", {
@@ -14,18 +21,65 @@ export const useStore = defineStore("main", {
     return {
       validWordList,
       solutionWordList,
+      todaysWord: solutionWordList[2112],
       currentWordAsArray: [],
       enteredWords: [],
+      matchColors: new Array(6)
+        .fill(undefined)
+        .map((x) => new Array(5).fill(" ")),
+      acceptingInputs: true,
     };
   },
   getters: {
-    todaysWord: (state) => state.solutionWordList[0],
+    todaysWordAsArray(state): string[] {
+      return state.todaysWord.split("");
+    },
+    todaysWordAsMap(state): IWordMap {
+      const wordMap: IWordMap = {};
+      for (const letter of state.todaysWord) {
+        wordMap[letter] = this.todaysWordAsArray.filter(
+          (x) => x === letter
+        ).length;
+      }
+      return wordMap;
+    },
   },
   actions: {
-    evaluateWord(wordArray: string[]) {
-      this.enteredWords.push(wordArray.join(""));
+    checkIfGameOver(): boolean {
+      if (this.enteredWords.length === 6) {
+        alert("Game Over!");
+        this.acceptingInputs = false;
+      }
+      return false;
     },
-    sendKey(value: string) {
+    evaluateWord(enteredWord: string[]): void {
+      enteredWord = enteredWord.map((letter) => letter.toLowerCase());
+      const wordMap: IWordMap = { ...this.todaysWordAsMap };
+      // first look for all perfect matches
+      for (let i = 0; i < 5; i++) {
+        if (enteredWord[i] === this.todaysWord[i]) {
+          wordMap[enteredWord[i]]--;
+          this.matchColors[this.enteredWords.length][i] = "green";
+        }
+      }
+      for (let i = 0; i < 5; i++) {
+        // perfect matches were already handled previously
+        if (this.matchColors[this.enteredWords.length][i] === "green") continue;
+        // letters not contained in solution or superfluous letters
+        if (
+          !wordMap.hasOwnProperty(enteredWord[i]) ||
+          wordMap[enteredWord[i]] === 0
+        ) {
+          this.matchColors[this.enteredWords.length][i] = "gray";
+        } else {
+          // letters that exist in the word somewhere
+          this.matchColors[this.enteredWords.length][i] = "yellow";
+          wordMap[enteredWord[i]]--;
+        }
+      }
+      this.enteredWords.push(enteredWord.join(""));
+    },
+    sendKey(value: string): void {
       if (value === "ENTER") {
         if (this.currentWordAsArray.length < 5) alert("not enough letters");
         else {
@@ -34,8 +88,9 @@ export const useStore = defineStore("main", {
             alert("not in word list");
             return;
           }
-		  console.log(`added ${this.currentWordAsArray.join("")} to entered words!`)
+
           this.evaluateWord(this.currentWordAsArray);
+          this.checkIfGameOver();
           this.currentWordAsArray = [];
         }
       } else if (value === "DELETE") {
