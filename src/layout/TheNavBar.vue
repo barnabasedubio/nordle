a0f35f68dd7cd9d42e557e5e84279c398ec6d7c6
Barnabas Edubio <barny.edubio@gmail.com>
2022-06-21 19:07:19 +0200

    make navbar items more responsive

--- src/layout/TheNavBar.vue
+++ src/layout/TheNavBar.vue
@@ -4,21 +4,37 @@
       class="container mx-auto flex flex-row justify-between items-center h-12"
     >
       <div class="flex">
-        <div class="w-14">
-					<img src="../assets/github-svgrepo-com.svg" width="28" alt="GitHub">
-				</div>
-        <div class="w-14">
-          <img src="../assets/help-svgrepo-com.svg" width="30" alt="Help" />
+        <div class="w-10">
+          <img
+            class="cursor-pointer mx-auto"
+            src="../assets/github-svgrepo-com.svg"
+            width="28"
+            alt="GitHub"
+          />
+        </div>
+        <div class="w-10">
+          <img
+            class="cursor-pointer mx-auto"
+            src="../assets/help-svgrepo-com.svg"
+            width="30"
+            alt="Help"
+          />
         </div>
       </div>
 
       <div class="w-24 title text-3xl text-nord4">Nordle</div>
       <div class="flex">
-        <div class="w-14">
-          <img src="../assets/stats-svgrepo-com.svg" width="28" alt="Stats" />
-        </div>
-        <div class="w-14">
+        <div class="w-10">
           <img
+            class="cursor-pointer mx-auto"
+            src="../assets/stats-svgrepo-com.svg"
+            width="28"
+            alt="Stats"
+          />
+        </div>
+        <div class="w-10">
+          <img
+            class="cursor-pointer mx-auto"
             src="../assets/settings-svgrepo-com.svg"
             width="28"
             alt="Settings"
@@ -31,6 +47,6 @@
 
 <style scoped>
 .title {
-	font-family: "Paytone One";
+  font-family: "Paytone One";
 }
 </style>
