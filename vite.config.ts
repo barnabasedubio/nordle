932a8ba53f4ce26035ba88fd84f19d1490df6baa
Barnabas Edubio <barny.edubio@gmail.com>
2022-07-17 22:44:22 +0200

    remote unused resolve statement

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-	"@": path.resolve(__dirname, './src'),
-  }
-})
+});
